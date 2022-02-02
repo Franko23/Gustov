b039d347b0cee0c8731429a1289f466a9bf5f60b
Franko <franko.2365@gmail.com>
2022-02-02 18:27:22 -0400

    Editar y eliminar registo de platos

--- Gustov/Controllers/HomeController.cs
+++ Gustov/Controllers/HomeController.cs
@@ -15,18 +15,18 @@ namespace Gustov.Controllers
         private gustovEntities db = new gustovEntities();
 
         MySql.Data.MySqlClient.MySqlConnection conn;
-        string connection = ConfigurationManager.ConnectionStrings["gustovEntities"].ConnectionString;
+        string connection = ConfigurationManager.ConnectionStrings["GUSTOVConnection"].ConnectionString;
 
         public ActionResult Index()
         {
             return View();
         }
 
-       public JsonResult ListarMenu()
+        public JsonResult ListarMenu()
         {
 
             var listaMenu = (from p in db.platos
-                             
+
                              select new
                              {
                                  p.id_plato,
@@ -37,41 +37,76 @@ namespace Gustov.Controllers
             return new JsonResult { Data = listaMenu, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
-        public JsonResult GetMenu()
-        {
-            List<platos> listaPlatos = new List<platos>();
-            conn = new MySql.Data.MySqlClient.MySqlConnection(connection);
-            conn.Open();
-            MySqlCommand cmd = new MySqlCommand();
-            cmd.CommandTimeout = 60;
-            cmd.CommandText = @"SELECT id_plato, nombre_plato, cantidad_plato
-                          FROM plato";
-            cmd.Connection = conn;
-            cmd.CommandType = CommandType.Text;
-            MySqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                platos p = new platos();
-                p.id_plato = reader.GetInt32(0);
-                p.nombre_plato = reader.GetString(1);
-                p.cantidad_plato = reader.GetInt32(2);
-                
-                listaPlatos.Add(p);
-            }
-            conn.Close();
-            return new JsonResult { Data = listaPlatos, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
-        }
-
         public JsonResult insertarPlato(platos platos)
         {
-            
 
             // Insertar Nuevo plato
-            
+
             db.platos.Add(platos);
             db.SaveChanges();
 
-            return new JsonResult { Data = "Guardado", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            return new JsonResult { Data = platos.nombre_plato + " agregado correctamente", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult getPlato(int id_plato)
+        {
+
+            var plato = (from p in db.platos
+                         where p.id_plato == id_plato
+
+                         select new
+                         {
+                             p.id_plato,
+                             p.nombre_plato,
+                             p.cantidad_plato
+                         }).ToList();
+
+            return new JsonResult { Data = plato, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+
+        }
+
+        public JsonResult editarPlato(platos plato)
+        {
+
+            platos pl = db.platos.Where(p => p.id_plato == plato.id_plato).First();
+           
+
+            string mensaje = plato.nombre_plato + " guardado correctamente";
+
+            if (pl != null)
+            {
+                pl.nombre_plato = plato.nombre_plato;
+                pl.cantidad_plato = plato.cantidad_plato;
+                db.SaveChanges();
+            }
+            else
+            {
+                mensaje = plato.nombre_plato + " no guardado";
+            }
+
+            return new JsonResult { Data = mensaje, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        public JsonResult eliminarPlato(int id_plato, string nombre_plato)
+        {
+
+            platos plato = db.platos.First(p => p.id_plato == id_plato);
+            string mensaje = nombre_plato + " eliminado";
+
+            if (plato != null)
+            {
+                db.platos.Remove(plato);
+                db.SaveChanges();
+            }
+            else
+            {
+                mensaje = nombre_plato +" no eliminado";
+            }
+
+
+            return new JsonResult { Data = mensaje, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
         }
 
     }
