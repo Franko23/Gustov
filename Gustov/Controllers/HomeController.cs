fb39517cab5f1e76b38c8188f936bceed6c2165d
FranKo <franko.2365@gmail.com>
2022-02-02 03:39:32 -0400

    Creación de la primera interfaz de prueba para recuperar datos (Read)

--- Gustov/Controllers/HomeController.cs
+++ Gustov/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Gustov.Models;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +12,55 @@ namespace Gustov.Controllers
 {
     public class HomeController : Controller
     {
+        private gustovEntities db = new gustovEntities();
+
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        string connection = ConfigurationManager.ConnectionStrings["gustovEntities"].ConnectionString;
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+       public JsonResult ListarMenu()
         {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+            var listaMenu = (from p in db.platos
+                             
+                             select new
+                             {
+                                 p.id_plato,
+                                 p.nombre_plato,
+                                 p.cantidad_plato
+                             }).ToList();
+
+            return new JsonResult { Data = listaMenu, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
-        public ActionResult Contact()
+        public JsonResult GetMenu()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            List<platos> listaPlatos = new List<platos>();
+            conn = new MySql.Data.MySqlClient.MySqlConnection(connection);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandTimeout = 60;
+            cmd.CommandText = @"SELECT id_plato, nombre_plato, cantidad_plato
+                          FROM plato";
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                platos p = new platos();
+                p.id_plato = reader.GetInt32(0);
+                p.nombre_plato = reader.GetString(1);
+                p.cantidad_plato = reader.GetInt32(2);
+                
+                listaPlatos.Add(p);
+            }
+            conn.Close();
+            return new JsonResult { Data = listaPlatos, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
+
     }
 }
